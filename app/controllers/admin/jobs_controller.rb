1ce5d45134ca4fe3e7b970e26c4140d27038ebe9
liubingjie <breeder1121@hotmail.com>
2017-11-09 17:03:49 +0800

    admin_job_CRUD

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,8 @@
-class JobsController < ApplicationController
+class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = Job.all
   end
 
   def show
@@ -15,7 +16,7 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
@@ -28,7 +29,7 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to job_path
+      redirect_to admin_jobs_path
     else
       render :edit
     end
@@ -37,7 +38,8 @@ class JobsController < ApplicationController
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
-    redirect_to jobs_path
+
+    redirect_to admin_jobs_path
   end
 
   private
