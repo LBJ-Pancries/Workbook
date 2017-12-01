335230127a809c9aeb653efac4d0b9e90098b2b3
liubingjie <breeder1121@gmail.com>
2017-12-01 10:26:35 +0800

    Add validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,31 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-    redirect_to @post
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
