3f39e4ed8d39ebb609313a7d15752d67850fed10
liubingjie <breeder1121@gmail.com>
2017-12-02 14:07:24 +0800

    Add user and finish styles

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   has_many :ingredients
   has_many :directions
+  belongs_to :user
 
   accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['step'].blank? }, allow_destroy: true
