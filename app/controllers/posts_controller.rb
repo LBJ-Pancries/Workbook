e35c59560190bfa5df78cf61a91497f3f1d1d896
liubingjie <breeder1121@gmail.com>
2017-12-06 14:46:12 +0800

    Add and setup voting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -30,6 +30,16 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to root_path
+  end
+
+  def upvote
+    @post.upvote_by current_user
+    redirect_to @post
+  end
+
+  def downvote
+    @post.downvote_by current_user
+    redirect_to @post
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
-  
+  acts_as_votable
+
   has_attached_file :image, styles: { medium: "700*500#", small: "350*250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
