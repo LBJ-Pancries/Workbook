1d2f57147cc2364b741713c6bcf7dfdb0dac041c
liubingjie <breeder1121@gmail.com>
2017-12-06 13:12:27 +0800

    Add create destroy ability for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :link, :description))
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:title, :link, :description))
+    redirect_to @post
+  end
+end
