fea3eda8c9751271ab2475e287c5d1f8fcd60e13
liubingjie <breeder1121@gmail.com>
2017-12-06 13:53:15 +0800

    Setup poperclip for image uploading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -9,22 +10,34 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.build(params.require(:post).permit(:title, :link, :description))
+    @post = current_user.posts.build(post_params)
     @post.save
     redirect_to @post
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post).permit(:title, :link, :description))
+    @post.update(post_params)
     redirect_to @post
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :description, :image)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "700*500#", small: "350*250#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
