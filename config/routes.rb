86dbec6230f271e56bacd3338a5a5faad164fb1b
liubingjie <breeder1121@gmail.com>
2017-12-15 18:02:50 +0800

    jQuery

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
     member do
       post "like" => "posts#like"
       post "unlike" => "posts#unlike"
+      post "toggle_flag" => "posts#toggle_flag"
       post "collect" => "posts#collect"
       post "cancel" => "posts#cancel"
+      post "rate" => "posts#rate"
     end
   end
 
